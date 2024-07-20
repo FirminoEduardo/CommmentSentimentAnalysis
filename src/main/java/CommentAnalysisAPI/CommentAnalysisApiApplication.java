2221309d5441abc918503d0f1e38e7ffe9e52c38
Eduardo Firmino <eduardofirmino@MacBook-Air-de-Eduardo.local>
2024-07-20 11:32:21 -0300

    First commit, with spring dependencies

---
+++ src/main/java/CommentAnalysisAPI/CommentAnalysisApiApplication.java
@@ -0,0 +1,13 @@
+package CommentAnalysisAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentAnalysisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentAnalysisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CommentAnalysisAPI/CommentAnalysisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package CommentAnalysisAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentAnalysisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
