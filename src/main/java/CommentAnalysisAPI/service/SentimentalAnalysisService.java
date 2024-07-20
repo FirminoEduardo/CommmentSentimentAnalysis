137b0d5caf85fd7e5e278abd920ba6fef3045d7a
Eduardo Firmino <eduardofirmino@MacBook-Air-de-Eduardo.local>
2024-07-20 14:15:51 -0300

    Sentiment analysis service

---
+++ src/main/java/CommentAnalysisAPI/service/SentimentalAnalysisService.java
@@ -0,0 +1,22 @@
+package CommentAnalysisAPI.service;
+
+import CommentAnalysisAPI.model.Comment;
+import CommentAnalysisAPI.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+public class SentimentalAnalysisService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public Comment analyzeSentiment(Comment comment) {
+        // Call to an external sentiment analysis API
+        RestTemplate restTemplate = new RestTemplate();
+        String apiUrl = "https://api.sentimentanalysis.com/analyze";
+        String sentiment = restTemplate.postForObject(apiUrl, comment.getText(), String.class);
+
+        comment.setSentiment(sentiment);
+        return commentRepository.save(comment);
+    }
+}
