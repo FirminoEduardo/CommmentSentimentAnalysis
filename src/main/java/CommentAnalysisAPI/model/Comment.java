f6a8a99b1be622af4c8935e230793d36602349c1
Eduardo Firmino <eduardofirmino@MacBook-Air-de-Eduardo.local>
2024-07-20 14:07:55 -0300

    Creation of data model

---
+++ src/main/java/CommentAnalysisAPI/model/Comment.java
@@ -0,0 +1,17 @@
+package CommentAnalysisAPI.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    private String sentiment;
+}
